378c471752383c42b0222a052938c8008733d9a9
Anatoly Mironov <mirontoli@gmail.com>
2015-12-06 00:00:20 +0100

    Add HelloEstimote

---
+++ HelloEstimote/HelloEstimote/ObjCBridge.h
@@ -0,0 +1,14 @@
+//
+//  ObjCBridge.h
+//  HelloEstimote
+//
+//  Created by Anatoly Mironov on 05/12/15.
+//  Copyright © 2015 A Chuvash Guy. All rights reserved.
+//
+
+#ifndef ObjCBridge_h
+#define ObjCBridge_h
+
+
+#endif /* ObjCBridge_h */
+#import <EstimoteSDK/EstimoteSDK.h>
