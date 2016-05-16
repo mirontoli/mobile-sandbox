59909864c8162abee037fd36a7fd59ed6fc6bd5c
Anatoly Mironov <mirontoli@gmail.com>
2016-05-16 23:23:29 +0200

    add old helloworlds

---
+++ Paging/Paging/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Paging
+//
+//  Created by Anatoly Mironov on 10/01/16.
+//  Copyright © 2016 A Chuvash Guy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
